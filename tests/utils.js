3640c25aa161ca5c2bd3f1d0a87d18ee0e6e823b
Chris Talkington <chris@talkingtontech.com>
2013-01-03 21:57:06 -0600

    javascript timezones really suck for testing.

--- tests/utils.js
+++ tests/utils.js
@@ -2,11 +2,23 @@ var utils = require('../lib/util/utils');
 
 var date1 = new Date('Jan 03 2013 14:26:38 GMT');
 var octal1 = 12071312436;
-var dos1 = 1109607251;
+var dos1 = 1109619539;
 
 var date2 = new Date('Jan 03 2013 12:24:36 GMT-0400');
 var octal2 = 12071330304;
-var dos2 = 1109611282;
+var dos2 = 1109623570;
+
+var offset = date1.getTimezoneOffset();
+
+function adjustByOffset(d) {
+  if (offset >= 1) {
+    d.setMinutes(d.getMinutes() - offset);
+  } else {
+    d.setMinutes(d.getMinutes() + Math.abs(offset));
+  }
+
+  return d;
+}
 
 module.exports = {
   octalDateTime: function(test) {
@@ -40,11 +52,11 @@ module.exports = {
   dosDateTime: function(test) {
     test.expect(2);
 
-    var actual = utils.dosDateTime(date1);
+    var actual = utils.dosDateTime(date1, true);
     var expected = dos1;
     test.equal(actual, expected);
 
-    actual = utils.dosDateTime(date2);
+    actual = utils.dosDateTime(date2, true);
     expected = dos2;
     test.equal(actual, expected);
 
@@ -54,11 +66,11 @@ module.exports = {
   convertDateTimeDos: function(test) {
     test.expect(2);
 
-    var actual = utils.convertDateTimeDos(dos1).toUTCString();
+    var actual = adjustByOffset(utils.convertDateTimeDos(dos1)).toUTCString();
     var expected = date1.toUTCString();
     test.equal(actual, expected);
 
-    actual = utils.convertDateTimeDos(dos2).toUTCString();
+    actual = adjustByOffset(utils.convertDateTimeDos(dos2)).toUTCString();
     expected = date2.toUTCString();
     test.equal(actual, expected);
 
