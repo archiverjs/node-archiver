d4036f31a84fc9db450a79dd9d7d4e28d478c5aa
Chris Talkington <chris@talkingtontech.com>
2013-02-12 18:33:26 -0600

    bench: support passing zip compression level.

--- benchmark/simple/pack-zip.js
+++ benchmark/simple/pack-zip.js
@@ -11,17 +11,39 @@ var binaryBuffer = common.binaryBuffer;
 var BITS_IN_BYTE = 1024;
 var BITS_IN_MBYTE = BITS_IN_BYTE * 1024;
 
-var archive = archiver('zip');
-var file;
+var file = false;
+var level = 1;
 
-if (!process.argv[2]) {
+if (process.argv[2]) {
+  if (isNaN(parseInt(process.argv[2], 10))) {
+    file = process.argv[2];
+
+    if (process.argv[3]) {
+      level = parseInt(process.argv[3], 10);
+
+      if (level > 9) {
+        level = 1;
+      }
+    }
+  } else {
+    level = parseInt(process.argv[2], 10);
+  }
+}
+
+var archive = archiver('zip', {
+  zlib: {
+    level: level
+  }
+});
+
+if (file === false) {
   mkdir.sync('tmp');
 
-  file = 'tmp/10mb.dat';
-  fs.writeFileSync(file, binaryBuffer(BITS_IN_MBYTE * 10));
-} else {
-  file = process.argv[2];
+  file = 'tmp/20mb.dat';
+  fs.writeFileSync(file, binaryBuffer(BITS_IN_MBYTE * 20));
 }
+
+console.log('zlib level: ' + level);
 
 archive
   .addFile(fs.createReadStream(file), { name: 'large file' })
@@ -29,8 +51,8 @@ archive
 
 var bench = streamBench({
   logReport: true,
-  interval:  500,
-  dump:      true
+  interval: 500,
+  dump: true
 });
 
 archive.pipe(bench);
