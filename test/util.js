0f4946e82617f97688f289e0af0c07eb710f30a9
Chris Talkington <chris@talkingtontech.com>
2014-01-10 21:49:44 -0600

    no need to double sanitizePath. sanitize colons (ie full windows paths). fixes #56.

--- test/util.js
+++ test/util.js
@@ -180,6 +180,8 @@ describe('utils', function() {
     describe('sanitizePath(filepath)', function() {
       it('should sanitize filepath', function() {
         assert.equal(utils.sanitizePath('\\this/path//file.txt'), 'this/path/file.txt');
+        assert.equal(utils.sanitizePath('/this/path/file.txt'), 'this/path/file.txt');
+        assert.equal(utils.sanitizePath('c:\\this\\path\\file.txt'), 'c/this/path/file.txt');
       });
     });
 
