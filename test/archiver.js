2a5fa09b985cb4e8b2a508d046f662ecdcc74ce8
Chris Talkington <chris@talkingtontech.com>
2014-04-04 12:32:42 -0500

    test: core tests for append.

--- test/archiver.js
+++ test/archiver.js
@@ -14,6 +14,8 @@ var binaryBuffer = common.binaryBuffer;
 
 var archiver = require('../lib/archiver');
 
+var testBuffer = binaryBuffer(1024 * 16);
+
 var testDate = new Date('Jan 03 2013 14:26:38 GMT');
 var testDate2 = new Date('Feb 10 2013 10:24:42 GMT');
 
@@ -23,6 +25,58 @@ describe('archiver', function() {
   });
 
   describe('core', function() {
+    describe('#append', function() {
+      var actual;
+
+      before(function(done) {
+        var archive = archiver('json');
+        var testStream = new WriteStream('tmp/append.json');
+
+        testStream.on('close', function() {
+          actual = common.readJSON('tmp/append.json');
+          done();
+        });
+
+        archive.pipe(testStream);
+
+        archive
+          .append(testBuffer, { name: 'buffer.txt', date: testDate })
+          .append(fs.createReadStream('test/fixtures/test.txt'), { name: 'stream.txt', date: testDate })
+          .append(null, { name: 'directory/', date: testDate })
+          .finalize();
+      });
+
+      it('should append buffer', function() {
+        assert.isArray(actual);
+        assert.propertyVal(actual[0], 'name', 'buffer.txt');
+        assert.propertyVal(actual[0], 'type', 'file');
+        assert.propertyVal(actual[0], 'date', '2013-01-03T14:26:38.000Z');
+        assert.propertyVal(actual[0], 'mode', 420);
+        assert.propertyVal(actual[0], 'crc32', 3893830384);
+        assert.propertyVal(actual[0], 'size', 16384);
+      });
+
+      it('should append stream', function() {
+        assert.isArray(actual);
+        assert.propertyVal(actual[1], 'name', 'stream.txt');
+        assert.propertyVal(actual[1], 'type', 'file');
+        assert.propertyVal(actual[1], 'date', '2013-01-03T14:26:38.000Z');
+        assert.propertyVal(actual[1], 'mode', 420);
+        assert.propertyVal(actual[1], 'crc32', 585446183);
+        assert.propertyVal(actual[1], 'size', 19);
+      });
+
+      it('should append directory', function() {
+        assert.isArray(actual);
+        assert.propertyVal(actual[2], 'name', 'directory/');
+        assert.propertyVal(actual[2], 'type', 'directory');
+        assert.propertyVal(actual[2], 'date', '2013-01-03T14:26:38.000Z');
+        assert.propertyVal(actual[2], 'mode', 493);
+        assert.propertyVal(actual[2], 'crc32', 0);
+        assert.propertyVal(actual[2], 'size', 0);
+      });
+    });
+
     describe('#file', function() {
       var actual;
 
