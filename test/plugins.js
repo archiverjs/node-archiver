00916219f3a8092ea22349b0059638d2e1cc9a46
Chris Talkington <chris@talkingtontech.com>
2014-12-28 01:18:49 -0600

    basic tests against built-in format plugins.

---
+++ test/plugins.js
@@ -0,0 +1,141 @@
+/*global before,describe,it */
+var fs = require('fs');
+var assert = require('chai').assert;
+var mkdir = require('mkdirp');
+var tar = require('tar');
+var unzip = require('unzip2');
+
+var archiver = require('../lib/archiver');
+var helpers = require('./helpers');
+var binaryBuffer = helpers.binaryBuffer;
+
+var testBuffer = binaryBuffer(1024 * 16);
+var testDate = new Date('Jan 03 2013 14:26:38 GMT');
+var testDate2 = new Date('Feb 10 2013 10:24:42 GMT');
+
+var win32 = process.platform === 'win32';
+
+describe('plugins', function() {
+  before(function() {
+    mkdir.sync('tmp');
+
+    if (!win32) {
+      fs.chmodSync('test/fixtures/executable.sh', 0777);
+    }
+  });
+
+  describe('tar', function() {
+    describe('#append', function() {
+      var actual = [];
+      var archive;
+      var entries = {};
+
+      before(function(done) {
+        archive = archiver('tar');
+        var testStream = tar.Parse();
+
+        testStream.on('entry', function(entry) {
+          actual.push(entry.path);
+          entries[entry.path] = entry;
+        });
+
+        testStream.on('end', function() {
+          done();
+        });
+
+        archive.pipe(testStream);
+
+        archive
+          .append(testBuffer, { name: 'buffer.txt', date: testDate })
+          .append(fs.createReadStream('test/fixtures/test.txt'), { name: 'stream.txt', date: testDate })
+          .append(null, { name: 'directory/', date: testDate })
+          .finalize();
+      });
+
+      it('should append multiple entries', function() {
+        assert.isArray(actual);
+        assert.lengthOf(actual, 3);
+      });
+
+      it('should append buffer', function() {
+        assert.property(entries, 'buffer.txt');
+        assert.propertyVal(entries['buffer.txt'].props, 'path', 'buffer.txt');
+        assert.propertyVal(entries['buffer.txt'].props, 'type', '0');
+        assert.propertyVal(entries['buffer.txt'].props, 'mode', 420);
+        assert.propertyVal(entries['buffer.txt'].props, 'size', 16384);
+      });
+
+      it('should append stream', function() {
+        assert.property(entries, 'stream.txt');
+        assert.propertyVal(entries['stream.txt'].props, 'path', 'stream.txt');
+        assert.propertyVal(entries['stream.txt'].props, 'type', '0');
+        assert.propertyVal(entries['stream.txt'].props, 'mode', 420);
+        assert.propertyVal(entries['stream.txt'].props, 'size', 19);
+      });
+
+      it('should append directory', function() {
+        assert.property(entries, 'directory/');
+        assert.propertyVal(entries['directory/'].props, 'path', 'directory/');
+        assert.propertyVal(entries['directory/'].props, 'type', '5');
+        assert.propertyVal(entries['directory/'].props, 'mode', 493);
+        assert.propertyVal(entries['directory/'].props, 'size', 0);
+      });
+    });
+  });
+
+  describe('zip', function() {
+    describe('#append', function() {
+      var actual = [];
+      var archive;
+      var entries = {};
+
+      before(function(done) {
+        archive = archiver('zip');
+        var testStream = unzip.Parse();
+
+        testStream.on('entry', function(entry) {
+          actual.push(entry.path);
+          entries[entry.path] = entry;
+          entry.autodrain();
+        });
+
+        testStream.on('close', function() {
+          done();
+        });
+
+        archive.pipe(testStream);
+
+        archive
+          .append(testBuffer, { name: 'buffer.txt', date: testDate })
+          .append(fs.createReadStream('test/fixtures/test.txt'), { name: 'stream.txt', date: testDate })
+          .append(null, { name: 'directory/', date: testDate })
+          .finalize();
+      });
+
+      it('should append multiple entries', function() {
+        assert.isArray(actual);
+        assert.lengthOf(actual, 3);
+      });
+
+      it('should append buffer', function() {
+        assert.property(entries, 'buffer.txt');
+        assert.propertyVal(entries['buffer.txt'], 'path', 'buffer.txt');
+        assert.propertyVal(entries['buffer.txt'], 'type', 'File');
+        assert.propertyVal(entries['buffer.txt'], 'size', 16384);
+      });
+
+      it('should append stream', function() {
+        assert.property(entries, 'stream.txt');
+        assert.propertyVal(entries['stream.txt'], 'path', 'stream.txt');
+        assert.propertyVal(entries['stream.txt'], 'type', 'File');
+        assert.propertyVal(entries['stream.txt'], 'size', 19);
+      });
+
+      it('should append directory', function() {
+        assert.property(entries, 'directory/');
+        assert.propertyVal(entries['directory/'], 'path', 'directory/');
+        assert.propertyVal(entries['directory/'], 'type', 'Directory');
+      });
+    });
+  });
+});
