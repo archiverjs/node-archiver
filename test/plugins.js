0a9ee060e22b8aab53450d7128029ad5b801530d
Jesse Swidler <jesse@udacity.com>
2017-10-27 15:36:14 -0700

    Fix relative symlink paths

--- test/plugins.js
+++ test/plugins.js
@@ -23,8 +23,8 @@ describe('plugins', function() {
     if (!win32) {
       fs.chmodSync('test/fixtures/executable.sh', 0777);
       fs.chmodSync('test/fixtures/directory/subdir/', 0755);
-      fs.symlinkSync('test/fixtures/directory/level0.txt', 'test/fixtures/directory/subdir/level0link.txt');
-      fs.symlinkSync('test/fixtures/directory/subdir/subsub/', 'test/fixtures/directory/subdir/subsublink');
+      fs.symlinkSync('../level0.txt', 'test/fixtures/directory/subdir/level0link.txt');
+      fs.symlinkSync('subsub/', 'test/fixtures/directory/subdir/subsublink');
     } else {
       fs.writeFileSync('test/fixtures/directory/subdir/level0link.txt', '../level0.txt');
       fs.writeFileSync('test/fixtures/directory/subdir/subsublink', 'subsub');
