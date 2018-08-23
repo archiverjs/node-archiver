34cd7b5811c836c0dc27e6b4ced59f8eaa569f6c
Chris Talkington <chris@talkingtontech.com>
2018-08-23 11:48:50 -0500

    update jsdoc and related deploy script.

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_REPO_SLUG" != "archiverjs/node-archiver" ] || [ "$TRAVIS_PULL_REQU
   exit 0
 fi
 
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_NODE_VERSION" != "4" ]; then
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_NODE_VERSION" != "6" ]; then
   exit 0
 fi
 
