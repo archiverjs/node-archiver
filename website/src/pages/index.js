f3e40083dbd7a67e8f924cdfde252188223b29ab
Chris Talkington <chris@talkingtontech.com>
2021-01-20 22:11:36 -0600

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,7 +9,7 @@ import styles from './styles.module.css';
 const features = [
   {
     title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    imageUrl: 'img/logo.svg',
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
@@ -19,7 +19,7 @@ const features = [
   },
   {
     title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    imageUrl: img/logo.svg',
     description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -29,7 +29,7 @@ const features = [
   },
   {
     title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    imageUrl: 'img/logo.svg',
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
