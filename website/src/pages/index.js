167406ffc0daa97eb67795d5148a18a3d6ba5524
Chris Talkington <chris@talkingtontech.com>
2021-01-23 14:25:52 -0600

    Update index.js

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -59,8 +59,8 @@ export default function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
